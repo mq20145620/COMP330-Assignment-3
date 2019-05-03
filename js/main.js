e58e81638e117dd6bfc14835915bd979cb690f69
Malcolm Ryan <malcolm.ryan@mq.edu.au>
2019-05-03 14:49:58 +1000

    Framework files

---
+++ js/main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+function run(level) {
+    // YOUR CODE GOES HERE 
+}    
+
+/**
+ * Load the selected level file and run it.
+ * @param {*} e 
+ */
+function onFileSelect(e) {
+    let files = e.target.files; 
+
+    if (files.length > 0) {
+        let reader = new FileReader();
+
+        reader.onload = function(e) {
+            let json = JSON.parse(e.target.result);
+            run(level);
+        };
+        
+        reader.readAsText(files[0]);
+    }
+}
+
+function main() {
+    // Check for the various File API support.
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        document.getElementById('files').addEventListener('change', onFileSelect, false);
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+        return;
+    }
+
+}
